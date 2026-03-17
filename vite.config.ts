829494a6da83fbdf0a6e6469a853c1063134f799
DaniyarDii <118101492+DaniyarDii@users.noreply.github.com>
2026-03-17 17:56:56 +0500

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/astana-boutique-style/',
   server: {
     host: "::",
     port: 8080,
